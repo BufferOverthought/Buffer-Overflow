b6fc54eabb7f7a620446f20fd92c6d82a1806d7d
BufferOverthought <tpgiovanni@yahoo.com>
2025-06-06 11:21:06 -0400

    Update fuzzer.py

--- fuzzer.py
+++ fuzzer.py
@@ -1,11 +1,14 @@
 import requests
 
-url = "http://localhost:5000/"
+url = "http://localhost:5001/" 
 with open("wordlist.txt") as f:
     for line in f:
         payload = line.strip()
-        r = requests.post(url, data={"username": payload})
-        if "Simulated shell" in r.text:
-            print(f"[!] Potential exploit found! Payload: {payload}\nResponse:\n{r.text}")
-        else:
-            print(f"Tested: {payload}")
+        try:
+            r = requests.post(url, data={"username": payload}, timeout=2)
+            if r.status_code == 500:
+                print(f"[!] CRASH! Payload: {payload} | Status: {r.status_code}")
+            else:
+                print(f"Tested: {payload} | Status: {r.status_code}")
+        except Exception as e:
+            print(f"[!] Exception or No response for Payload: {payload} | Error: {e}")
